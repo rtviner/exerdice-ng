9a29ff326ebc0481161402c9337204fc7898220f
Rebecca Viner <rtviner@gmail.com>
2020-03-11 13:35:55 -0600

    add total rounds info to round service

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -12,7 +12,10 @@ import { RoundService } from '../round.service';
 export class GameComponent implements OnInit {
   dieNumber: number;
   currentExercise: Exercise;
-
+  //  need to get totRounds from start component
+  //  need to keep track of current round num
+    // roundService rounds.length?
+  
   constructor(private exerciseService: ExerciseService, private roundService: RoundService) { }
 
   ngOnInit() {
@@ -22,13 +25,19 @@ export class GameComponent implements OnInit {
   getExercise(exNumber: number): void {
     this.exerciseService.getExercise(exNumber)
       .subscribe(exercise => this.currentExercise = exercise);
-
   }
 
   onPlay() {
-    this.dieNumber = Math.floor(Math.random() * 6) +1;
-    this.getExercise(this.dieNumber);
-    this.roundService.add(this.currentExercise.titles[0]);
+    //only want to do the following if currentRounds < totRounds
+
+    if (this.roundService.rounds.length < this.roundService.totalRounds) {
+      this.dieNumber = Math.floor(Math.random() * 6) +1;
+      this.getExercise(this.dieNumber);
+      this.roundService.add(this.currentExercise.titles[0]);
+    } else {
+      console.log("game over");
+    }
+    //else show game over message and game summary
   }
 
 }
--- src/app/round.service.ts
+++ src/app/round.service.ts
@@ -4,10 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RoundService {
+  totalRounds: number = 6;
   rounds: string[] = [];
 
+  setTotalRounds(numOfRounds: number) {
+    this.totalRounds = numOfRounds;
+  }
+
+  //having the if statement here prevents new rounds from being added to the rounds array but doesnt prevent the exercise component from showing the exercise information for additional rounds
   add(round: string) {
-    this.rounds.push(round);
+      this.rounds.push(round);
   }
   
   clear(){
