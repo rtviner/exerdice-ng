38fad37bd781244c4447c17349ab177797feed76
Rebecca Viner <rtviner@gmail.com>
2020-03-19 14:54:56 -0600

    add wobble animation and blockInitialAnimation to dice component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -25,6 +25,7 @@ import { LastRoundMessageComponent } from './last-round-message/last-round-messa
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule
   ],
