e5764484efcd6e9b820f0936d230fb6708ae19d1
Rebecca Viner <rtviner@gmail.com>
2020-03-20 12:53:44 -0600

    add rolls and currentIndex as class properties

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,13 +8,15 @@ import { RoundService } from '../round.service';
 @Component({
   selector: 'game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
 })
 export class GameComponent implements OnInit {
   diceClass: string;
   currentExercise: Exercise;
+  currentIndex: number;
   roundNumber: number;
   roundsComplete = false;
+  rolls = new Map<number, number>();
 
   constructor(private router: Router, private exerciseService: ExerciseService, private roundService: RoundService) { }
 
@@ -28,24 +30,38 @@ export class GameComponent implements OnInit {
   }
 
    onPlay() { 
-    this.roundNumber = this.roundService.getRoundNumber();
+    this.roundNumber = 
+      this.roundService.getRoundNumber();
  
+    if (this.roundNumber < this.roundService.totalRounds) {
+      let dieNumber = Math.floor(Math.random() * 6) +1;
+      //increment dieNumber in rolls
+      let rollFreq = this.rolls.get(dieNumber);
+      if (rollFreq && rollFreq < 3) {
+        this.rolls.set(dieNumber, rollFreq + 1);
+      }
+      if (!rollFreq) {
+        this.rolls.set(dieNumber, 1);
+      };
+
+      this.diceClass = `dice_${dieNumber}`;
+      this.getExercise(dieNumber);
+      //instead of [0] --> [rolls.get(dieNumber)]
+      this.currentIndex = this.rolls.get(dieNumber) - 1 || 0;
+
+      this.roundService.add(this.currentExercise.titles[this.currentIndex]);
+    }; 
     if (this.roundNumber === this.roundService.totalRounds -1) {
       this.roundsComplete = true;
     };
-
-    if (this.roundNumber < this.roundService.totalRounds) {
-      let dieNumber = Math.floor(Math.random() * 6) +1;
-      this.diceClass = `dice_${dieNumber}`;
-      this.getExercise(dieNumber);
-      this.roundService.add(this.currentExercise.titles[0]);
-    }; 
   }
 
   resetGame() {
     this.currentExercise = null;
+    this.currentIndex = null;
     this.roundsComplete = false;
     this.roundService.clear();
+    this.rolls = new Map<number, number>();
   }
   
   newGame() {
