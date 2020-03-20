285fcc205b81e1d416e1dd5e2ee4300a21f75633
Rebecca Viner <rtviner@gmail.com>
2020-03-20 12:55:36 -0600

    remove rolls property

--- src/app/mock-exercises.ts
+++ src/app/mock-exercises.ts
@@ -7,47 +7,41 @@ export const EXERCISES: Exercise[] = [
 		titles: ["Two-Handed Swings", "One-Arm Swings", "Double-Arm Swings"],
 		partnerExercise: "Rest",
 		setsReps: ["3 Sets of 10 Reps", "3 Sets of 10 Reps", "3 Sets of 10 Reps"],
-		tips: ["Pinch and Brace.","Lower legs vertical.", "Chest pushed out.", "Arms loose."],
-    rolls: 0 
+		tips: ["Pinch and Brace.","Lower legs vertical.", "Chest pushed out.", "Arms loose."]
   },
   { id: 2, 
     number: 2, 
 		titles: ["Single Floor Press", "Double Floor Press", "Heavier Double Floor Press"], 
 		partnerExercise: "TRX/Plank",
 		setsReps: ["3 Sets of 5 Reps Each Side", "3 Sets of 5 Reps", "2-3 Sets of 5 Reps"],
-		tips: ["Pinch and Brace.", "Wrists bent.", "Elbows tucked.", "Shoulders packed."],
-    rolls: 0
+		tips: ["Pinch and Brace.", "Wrists bent.", "Elbows tucked.", "Shoulders packed."]
   },
   { id: 3, 
     number: 3, 
 		titles: ["Clean and Press", "Clean and Seesaw Press", "Double Clean and Press"], 
 		partnerExercise: "Rest",
-		setsReps: ["1-2-3 Reps/Sets Ladder", "1-2-3 Reps/Sets Ladder", "1-2-3 Reps/Sets Ladder"],
-		tips: ["Pinch and Brace.", "Elbows tucked.", "Kettlebells close.", "Feet together to Press."],
-    rolls: 0
+		setsReps: ["3 sets of 1-2-3 Reps", "3 sets of 1-2-3 Reps", "3 sets of 1-2-3 Reps"],
+		tips: ["Pinch and Brace.", "Elbows tucked.", "Kettlebells close.", "Feet together to Press."]
   },
   { id: 4, 
     number: 4, 
 		titles: ["Quarter Getups", "Turkish Getups", "Turkish Getup Walk"], 
 		partnerExercise: "Rest",
-		setsReps: ["3 Reps 1 set", "1-2-3 Reps Ladder", "1-2-3 Reps Ladder"],
-		tips: ["Pinch and Brace.", "Eyes on the kettlebell.", "Wrist bent.", "Shoulder packed."],
-    rolls: 0
+		setsReps: ["3 Reps 1 set", "3 sets of 1-2-3 Reps", "3 sets of 1-2-3 Reps"],
+		tips: ["Pinch and Brace.", "Eyes on the kettlebell.", "Wrist bent.", "Shoulder packed."]
   },
   { id: 5, 
     number: 5, 
-		titles: ["Figure 8's 1 set", "Figure 8's 2 sets", "Figure 8's 3 sets"], 
+		titles: ["Figure 8's", "Figure 8's", "Figure 8's"], 
 		partnerExercise: "TRX/Planks",
-		setsReps: ["30 secs/Set", "30 secs/Set", "30 secs/Set"],
-		tips: ["Pinch and Brace.", "Eyes ahead.", "Moderate weight.", "Feet rooted."],
-    rolls: 0
+		setsReps: ["1 set of 30 secs", "2 sets of 30 secs", "3 sets of 30 secs"],
+		tips: ["Pinch and Brace.", "Eyes ahead.", "Moderate weight.", "Feet rooted."]
   },
   { id: 6, 
     number: 6, 
 		titles: ["Front Step Lunge", "Reverse Step Lunge", "Front Step Lunge - Racked Weight"],
 		partnerExercise: "Reverse Plank",
-		setsReps: ["12 Reps Alternating Legs 2 sets", "12 Reps Alternating Legs 2 sets", "12 Reps Alternating Legs 2 sets"], 
-		tips: ["Pinch and Brace.", "Feet together first.", "Stretch your leg out front.", "Hips even."],
-    rolls: 0
+		setsReps: ["2 sets of 12 Reps Alternating Legs", "2 sets of 12 Reps Alternating Legs", "2 sets of 12 Reps Alternating Legs"], 
+		tips: ["Pinch and Brace.", "Feet together first.", "Stretch your leg out front.", "Hips even."]
   }
 ];
