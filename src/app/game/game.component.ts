ad560dcc72f06e91e5a72c8c8ca2e1035ebf0a55
Rebecca Viner <rtviner@gmail.com>
2020-03-20 14:26:39 -0600

    cleanup onPlay function, move roll logic to rollDie function

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,11 +10,11 @@ import { RoundService } from '../round.service';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css'],
 })
+
 export class GameComponent implements OnInit {
   diceClass: string;
   currentExercise: Exercise;
   currentIndex: number;
-  roundNumber: number;
   roundsComplete = false;
   rolls = new Map<number, number>();
 
@@ -23,35 +23,41 @@ export class GameComponent implements OnInit {
   ngOnInit() {
     this.resetGame();
   }
+  
+  rollDie() {
+    let dieNumber = Math.floor(Math.random() * 6) +1;
+    this.diceClass = `dice_${dieNumber}`;
+    let rollFreq = this.rolls.get(dieNumber);
+    //increment roll freq for dieNumber if less than 3 bc there are only 3 variations for each exercise
+    if (rollFreq && rollFreq < 3) {
+      this.rolls.set(dieNumber, rollFreq + 1);
+    }
+    if (!rollFreq) {
+      this.rolls.set(dieNumber, 1);
+    };
+
+    return dieNumber;
+  }
 
   getExercise(exNumber: number): void {
     this.exerciseService.getExercise(exNumber)
       .subscribe(exercise => this.currentExercise = exercise);
   }
 
-   onPlay() { 
-    this.roundNumber = 
-      this.roundService.getRoundNumber();
+  onPlay() { 
+    let currentRound = 
+      this.roundService.getRoundNumber() + 1;
  
-    if (this.roundNumber < this.roundService.totalRounds) {
-      let dieNumber = Math.floor(Math.random() * 6) +1;
-      //increment dieNumber in rolls
-      let rollFreq = this.rolls.get(dieNumber);
-      if (rollFreq && rollFreq < 3) {
-        this.rolls.set(dieNumber, rollFreq + 1);
-      }
-      if (!rollFreq) {
-        this.rolls.set(dieNumber, 1);
-      };
-
-      this.diceClass = `dice_${dieNumber}`;
+    if (currentRound <= this.roundService.totalRounds) {
+      let dieNumber = this.rollDie();
       this.getExercise(dieNumber);
-      //instead of [0] --> [rolls.get(dieNumber)]
+      
       this.currentIndex = this.rolls.get(dieNumber) - 1 || 0;
 
       this.roundService.add(this.currentExercise.titles[this.currentIndex]);
     }; 
-    if (this.roundNumber === this.roundService.totalRounds -1) {
+    
+    if (currentRound === this.roundService.totalRounds) {
       this.roundsComplete = true;
     };
   }
