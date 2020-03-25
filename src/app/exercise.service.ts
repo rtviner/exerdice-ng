6beb231d9cc98405c4f9a957ee1ccc80a6cf16a0
Rebecca Viner <rtviner@gmail.com>
2020-03-25 16:16:13 -0600

    update methods to use http client

--- src/app/exercise.service.ts
+++ src/app/exercise.service.ts
@@ -1,21 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Exercise } from './exercise';
-import { EXERCISES } from './mock-exercises';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ExerciseService {
 
-  constructor() { }
+  private exercisesUrl = 'https://exerdice-backend.herokuapp.com/api/exercises';
 
-  getExercises(): Observable<Exercise[]> {
-    return of(EXERCISES);
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
   }
 
-  getExercise(number: number): Observable<Exercise> {
-    return of (EXERCISES.find(exercise => exercise.number === number));
+  getExercises(): Observable<Exercise[]> {
+    return this.http.get<Exercise[]>(this.exercisesUrl)
+      .pipe(
+        catchError(this.handleError<Exercise[]>('getExercises', []))
+      );
+  }
+
+  getExercise(id: number): Observable<Exercise> {
+    const url = `${this.exercisesUrl}/${id}`;
+    return this.http.get<Exercise>(url).pipe(
+      catchError(this.handleError<Exercise>(`getExercise id=${id}`))
+    );
   }
 }
