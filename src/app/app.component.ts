511bb1099d26567b5009e1bf98a301a78db102b3
Rebecca Viner <rtviner@gmail.com>
2020-03-27 12:44:44 -0600

    subscribe to message service on 1st load to wake up heroku api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from './message.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'exerdice-ng';
+
+  constructor(private messageService: MessageService) { }
+  
+  ngOnInit() {
+    this.messageService.getRandomEncouragement()
+    .subscribe(message => {
+      console.log(`api loaded, ${message.message}`);
+    });
+  }
 }
